b68a2439f90401491b1473d7a0cded5dc8fede2b
officialcody <sparshgoelt26@gmail.com>
2021-08-25 22:05:23 +0530

    modified Merge sort'

--- Sorting/MergeSort.js
+++ Sorting/MergeSort.js
@@ -1,53 +1,40 @@
-function merge(arr, l, m, r) {
-  var n1 = m - l + 1;
-  var n2 = r - m;
-  var L = new Array(n1);
-  var R = new Array(n2);
-
-  for (var i = 0; i < n1; i++) {
-    L[i] = arr[l + i];
+function mergeSort (array) {
+  if (array.length === 1) {
+    return array
   }
-
-  for (var j = 0; j < n2; j++){
-    R[j] = arr[m + 1 + j];
-  }
+  const length = array.length;
+  // Split Array in into right and left by defining a pivot
+  const middle = Math.floor(length / 2)
+  //left half
+  const left = array.slice(0, middle)
+  //right half
+  const right = array.slice(middle)
   
-  var i = 0;
-  var j = 0;
-  var k = l;
- 
-  while (i < n1 && j < n2) {
-    if (L[i] <= R[j]) {
-      arr[k] = L[i];
-      i++;
-    }
-    else {
-      arr[k] = R[j];
-      j++;
-    }
-    k++;
-  }
-
-  while (i < n1) {
-    arr[k] = L[i];
-    i++;
-    k++;
-  }
-
-  while (j < n2) {
-    arr[k] = R[j];
-    j++;
-    k++;
-  }
+  //merge the two halves using the next function
+  return merge(
+    mergeSort(left),
+    mergeSort(right)
+  )
 }
 
-function mergeSort(arr,l, r){
-  if(l>=r) return;
-  let m = l+ parseInt((r-l)/2);
-  mergeSort(arr,l,m);
-  mergeSort(arr,m+1,r);
-  merge(arr,l,m,r);
-  return arr
+function merge(left, right){
+  const result = [];
+  //keep an index count instead of using a loop to iterate
+  let leftIndex = 0;
+  let rightIndex = 0;
+  //append the lower value of the left or right indices 
+  while(leftIndex < left.length && 
+        rightIndex < right.length){
+     if(left[leftIndex] < right[rightIndex]){
+       result.push(left[leftIndex]);
+       leftIndex++;
+     } else{
+       result.push(right[rightIndex]);
+       rightIndex++
+    }
+  }  
+  //our concatenated and sorted array
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
 
 console.log(mergeSort([5,1,0,2,4,6,7,9], 0, 7))
